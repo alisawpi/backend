73143712357a855cc7db3e1cbe2eb3719d3cc795
Alisa Piippo <alisa.piippo@aalto.fi>
2020-11-02 18:41:12 +0200

    build

---
+++ build/precache-manifest.dde8a9ac5ea9f79e76acca9d5de6616d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27db8e25f2e2edbdd63775f487cc0d7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28a231655d68437b42ef",
+    "url": "/static/css/main.c26bcc3b.chunk.css"
+  },
+  {
+    "revision": "7427d3f266ccd97afbe1",
+    "url": "/static/js/2.442a0555.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.442a0555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a231655d68437b42ef",
+    "url": "/static/js/main.1f0852e1.chunk.js"
+  },
+  {
+    "revision": "fcfa87c657e9ad05204f",
+    "url": "/static/js/runtime-main.ef781745.js"
+  }
+]);
